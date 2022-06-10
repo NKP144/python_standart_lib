9ef5976429930041de35893af2da46d99b60e888
NKP144 <xachepurik@inbox.ru>
2022-06-10 07:16:55 +0300

    Add echo/client server with coroutine

---
+++ asyncio_test/asyncio_echo_server_coroutine.py
@@ -0,0 +1,53 @@
+import asyncio
+import logging
+import sys
+
+SERVER_ADDRESS = ('localhost', 10000)
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(name)s: %(message)s',
+    stream=sys.stderr,
+)
+log = logging.getLogger('main')
+
+
+async def echo(reader, writer):
+    address = writer.get_extra_info('peername')
+    log = logging.getLogger('echo_{}_{}'.format(*address))
+    log.debug('connection accepted')
+    while True:
+        data = await reader.read(128)
+        if data:
+            log.debug('received {!r}'.format(data))
+            writer.write(data)
+            await writer.drain()
+            log.debug('sent {!r}'.format(data))
+        else:
+            log.debug('closing')
+            writer.close()
+            return
+
+
+event_loop = asyncio.get_event_loop()
+
+# Создать сервер и завершить сопрограмму перед запуском реального цикла событий
+factory = asyncio.start_server(echo, *SERVER_ADDRESS)
+server = event_loop.run_until_complete(factory)
+log.debug('starting up on {} port {}'.format(*SERVER_ADDRESS))
+
+# Войти в бесконечный цикл событий для обработки всех соедининий
+try:
+    event_loop.run_forever()
+except KeyboardInterrupt:
+    pass
+finally:
+    log.debug('closing server')
+    server.close()
+    event_loop.run_until_complete(server.wait_closed())
+    log.debug('closing event loop')
+    event_loop.close()
+
+
+
+
